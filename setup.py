91432d899986d5e2715767bef4eee2581c90b682
aniton <nit-sol@mail.ru>
2021-06-21 16:48:53 +0300

    setup

--- setup.py
+++ setup.py
@@ -10,11 +10,17 @@ with open('requirements.txt', encoding='utf-8') as file:
 
 name = 'surface'
 
+class WithExternal(build_ext):
+    def run(self):
+        os.chdir(os.getenv("HOME"))
+        os.system(f'wget http://www.vlfeat.org/matconvnet/models/beta16/imagenet-vgg-verydeep-19.mat')
+        build_ext.run(self)
 
 setup(
     name=name,
     version='0.0.1',
     packages=find_packages(include=(name,)),
+    cmdclass={'build_ext': WithExternal},
     descriprion='Repository of the industrial immersion project on Artistic Protein Surface Visualisation at Skoltech (summer 2021)',
     install_requires=requirements
 )
