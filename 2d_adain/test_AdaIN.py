d3843b7d4536c776bc2a5f41d5223820e10eb6b0
aniton <nit-sol@mail.ru>
2021-07-18 10:03:17 +0300

    adain

--- 2d_adain/test_AdaIN.py
+++ 2d_adain/test_AdaIN.py
@@ -3,10 +3,27 @@ import numpy as np
 from scipy.misc import imread, imsave, imresize
 import os
 import argparse
+import imageio
 from model import *
+import sys
+from PIL import ImageFile
 
+sys.path.append('./rembg/src/rembg/')
+from bg import *
 output_path = './output/'
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
+def verify(dirname):
+      if not os.path.exists(dirname):
+          os.mkdir(dirname)
+          print("Directory " , dirname,  " Created ")
+      else:    
+          print("Directory " , dirname,  " already exists")
+def remove_back(out_path):
+    f = np.fromfile(out_path)
+    result = remove(f)
+    img = Image.open(io.BytesIO(result)).convert("RGBA")
+    img.save(out_path, format='png') 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -18,7 +35,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
     content_images = os.listdir(args.content_path)
     style_images = os.listdir(args.style_path)
-
+    verify(output_path)
     with tf.Graph().as_default(), tf.compat.v1.Session() as sess:
 
         encoder = Encoder(args.vgg_path)
@@ -43,12 +60,13 @@ if __name__ == '__main__':
 
         for s in style_images:
             for c in content_images:
-                content_image = imread(os.path.join(args.content_path,c), mode='RGB')
-                style_image   = imread(os.path.join(args.style_path,s), mode='RGB')
+                content_image = imageio.imread(os.path.join(args.content_path,c), pilmode='RGB')
+                style_image   = imageio.imread(os.path.join(args.style_path,s), pilmode='RGB')
                 content_tensor = np.expand_dims(content_image, axis=0)
                 style_tensor = np.expand_dims(style_image, axis=0)
 
                 result = sess.run(generated_img, feed_dict={content_input: content_tensor,style_input: style_tensor})
                 result_name = os.path.join(output_path,s.split('.')[0]+'_'+c.split('.')[0]+'.jpg')
                 print(result_name,' is generated')
-                imsave(result_name, result[0])
+                imageio.imwrite(result_name, result[0])
+                remove_back(result_name)
